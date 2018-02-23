c1d9ec07b7cd98f422bcb2222f96b20c796d8cb8
theoriginaljosh <theoriginaljosh@gmail.com>
2018-02-23 14:51:28 -0500

    adding some comment.s

--- nativescript-vue.d.ts
+++ nativescript-vue.d.ts
@@ -1,6 +1,9 @@
+// Typings for NativeScript-Vue
 declare module 'nativescript-vue' {
+	// import vue.js typings
 	import Vue from 'vue';
 
+	// creat a nativescript vue class that extends vue.js typings and adds the $start function.
 	class NativeScriptVue extends Vue {
 		/**
 		 * calls the NativeScript application.start() method
