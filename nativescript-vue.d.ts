d09f69a022bc478eff80115592c7a2840a03fddc
Josh Sommer <JoshDSommer@gmail.com>
2018-02-25 11:33:13 -0500

    adding typings registerElement
    Allows adding nativescript plugins.

--- nativescript-vue.d.ts
+++ nativescript-vue.d.ts
@@ -9,6 +9,14 @@ declare module 'nativescript-vue' {
 		 * calls the NativeScript application.start() method
 		 */
 		$start(): void;
+
+		/**
+		 * Registers NativeScript Plugin.
+		 * @param elementName Name of the element to use in your template
+		 * @param resolver  function to register the element
+		 */
+		static registerElement(elementName: string, resolver: function);
+
 		$http: any;
 	}
 
