6c23422eb5fcc3aefe20caeaa9ad02eb64a1cf39
theoriginaljosh <theoriginaljosh@gmail.com>
2018-02-27 07:56:38 -0500

    updating comment

--- nativescript-vue-router.d.ts
+++ nativescript-vue-router.d.ts
@@ -7,6 +7,7 @@ declare module 'vue-router/types/router' {
 	interface RouterOptions {
 		/**
 		 * NativeScript option
+		 * switches the router to use the navigation stack to navigate between pages
 		*/
 		pageRouting?: boolean;
 	}
