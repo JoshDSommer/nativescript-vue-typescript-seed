f0db998a66b3a6cea083cbbab3035c7e84c1af73
theoriginaljosh <theoriginaljosh@gmail.com>
2018-03-01 13:18:34 -0500

    Augment vue.js

---
+++ vue.d.ts
@@ -0,0 +1,18 @@
+//Augmenting Vue.js
+import Vue from 'vue';
+
+declare module "vue/types/vue" {
+	interface Vue {
+		/**
+		 * calls the NativeScript application.start() method
+		 */
+		$start(): void;
+
+		/**
+		 * Registers NativeScript Plugin.
+		 * @param elementName Name of the element to use in your template
+		 * @param resolver  function to register the element
+		 */
+		static registerElement(elementName: string, resolver: function);
+	}
+}
