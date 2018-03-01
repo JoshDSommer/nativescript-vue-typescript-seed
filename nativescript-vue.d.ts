29654584258b3a07b4743504904ef11acf825664
theoriginaljosh <theoriginaljosh@gmail.com>
2018-03-01 13:19:30 -0500

    removing the typings specific to ns-vue
    instead these will come from the augmented vue.js version

--- nativescript-vue.d.ts
+++ nativescript-vue.d.ts
@@ -3,19 +3,9 @@ declare module 'nativescript-vue' {
 	// import vue.js typings
 	import Vue from 'vue';
 
-	// creat a nativescript vue class that extends vue.js typings and adds the $start function.
+	// creat a nativescript vue class that extends vue.js
 	class NativeScriptVue extends Vue {
-		/**
-		 * calls the NativeScript application.start() method
-		 */
-		$start(): void;
 
-		/**
-		 * Registers NativeScript Plugin.
-		 * @param elementName Name of the element to use in your template
-		 * @param resolver  function to register the element
-		 */
-		static registerElement(elementName: string, resolver: function);
 	}
 
 	export = NativeScriptVue;
